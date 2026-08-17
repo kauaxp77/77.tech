e51af28cb75a2632382720026d6692259b328770
kauaxp77? <Wendesonkaua11@gmail.com>
2026-08-17 18:15:25 -0300

    fix(layout): isolate AdminSidebar via Next.js Route Groups

--- src/app/studio/[[...index]]/page.tsx
+++ src/app/studio/[[...index]]/page.tsx
@@ -1,20 +1,18 @@
 'use client'
 
-import { NextStudio } from 'next-sanity/studio'
+import dynamic from 'next/dynamic'
 import config from '../../../sanity/sanity.config'
-import Head from 'next/head'
+
+const NextStudioDynamic = dynamic(
+    () => import('next-sanity/studio').then((mod) => mod.NextStudio),
+    { ssr: false }
+)
 
 export default function StudioPage() {
     return (
-        <>
-            <Head>
-                {/* Next.js requires this to hide search engine crawlers from the Studio */}
-                <meta name="robots" content="noindex" />
-            </Head>
-            <div className="absolute inset-0 z-[9999] bg-white">
-                {/* Studio controls full Viewport */}
-                <NextStudio config={config} />
-            </div>
-        </>
+        <div className="absolute inset-0 z-[9999] bg-white">
+            {/* Sanity Studio Render */}
+            <NextStudioDynamic config={config} />
+        </div>
     )
 }
